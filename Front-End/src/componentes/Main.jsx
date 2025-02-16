6ba19432c1bd915f4a082cda30c1af21a7da2f5c
Alan Pereira <alanferreira041@hotmail.com>
2025-02-16 20:35:20 -0300

    feito atualizações para deploy

--- Front-End/src/componentes/Main.jsx
+++ Front-End/src/componentes/Main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ItemList from "./itemList";
+import ItemList from "./ItemList.jsx";
 import { artistArray } from "../assets/DataBase/artists";
 import { songsArray } from "../assets/DataBase/songs";
 
