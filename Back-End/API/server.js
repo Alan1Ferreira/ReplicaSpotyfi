05fbcbd04ae42410a1b34275818d59744fcc726f
Alan Pereira <alanferreira041@hotmail.com>
2025-02-16 20:21:53 -0300

    Feito alterações de variaveis de ambiente para deploy

--- Back-End/API/server.js
+++ Back-End/API/server.js
@@ -1,22 +1,33 @@
 import express from "express";
 import cors from "cors";
 import { db } from "./connect.js";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 const PORT = 3001;
 
 app.use(cors());
 
-app.get("/", (request, response) => {
+app.get("/API/", (request, response) => {
   response.send("Hello World");
 });
 
-app.get("/artists", async (request, response) => {
+app.get("/API/artists", async (request, response) => {
   response.send(await db.collection("artists").find({}).toArray());
 });
 
-app.get("/songs", async (request, response) => {
+app.get("/API/songs", async (request, response) => {
   response.send(await db.collection("songs").find({}).toArray());
+});
+
+app.use(express.static(path.join(__dirname, "../../Front-End/dist")));
+
+app.get("*", async (request, response) => {
+  response.sendFile(
+    express.static(path.join(__dirname, "../../Front-End/dist/index.html"))
+  );
 });
 
 app.listen(PORT, () => {
